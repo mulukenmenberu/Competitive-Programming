e482395ca1310f7a29958beda17a51f3415e605d
Muluken M <mulerapps@gmail.com>
2022-07-08 16:18:36 +0300

    selection sort algorithm

---
+++ SelectionSort.js
@@ -0,0 +1,22 @@
+
+function SelectionSort(arr=[]){
+    if(!arr || arr.length<=0) return "array is empty"
+    let arrayIsNumber = arr.every(item=>{
+        return typeof item === 'number'
+    })
+    if(!arrayIsNumber) return 'all array elements mut be numbers'
+for(let x=0;x<arr.length-2;x++){
+    let min = x
+    for(let y=x+1;y<arr.length-1;y++){
+        if(arr[y]<arr[min]){
+            min = y
+        }
+    }
+    let large = arr[x]
+    arr[x] = arr[min]
+    arr[min] = large
+}
+
+    return arr;
+}
+console.log(SelectionSort([1,3,5]))
